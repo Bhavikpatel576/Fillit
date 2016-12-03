349ccce3abc1d6c97f523001a4aa61511e2db9ab
Bhavik Patel <bpatel@e1z2r1p11.42.us.org>
2016-12-03 01:31:57 -0800

    update info

--- ft_fillit.c
+++ ft_fillit.c
@@ -13,8 +13,11 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include "libft.h"
+#include "fillit.h"
 
 #define BUF_SIZE 4000
+
+
 /* 
  * allowed functions exit, open, close, write, read, malloc, free
 */
@@ -46,7 +49,7 @@ char 	*read_information(char* file_name)
 {
 	char buf[BUF_SIZE + 1];
 	char *return_string;
-	int  read_file;
+	int  read_file;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 	int  bytes_read;
 
 	read_file = open(file_name, O_RDWR);
@@ -70,7 +73,7 @@ char 	*read_information(char* file_name)
 **	Is_valid will read in the tetrimino and check to see if it is valid
 **  if the information is valid return error prompt. Will count to see if a 4 x 4
 ** 	block has been passed through. Return true or false. Second step will be to 
-** 	see if tetrimino is a valid shape.
+** 	see if tetrimino is a valid shape. ****currently results in a false negative with single tets**
 */
 
 int	is_valid(char *file, size_t file_buffer)
@@ -80,8 +83,6 @@ int	is_valid(char *file, size_t file_buffer)
 
 	iter = 4;
 	j = -1;
-	//code will iterate through string 4 char, check for new line on 5th
-	// char <-will do this 4 times and check for 2 new lines
 	while (file_buffer > iter)
 	{
 		while (j++ < 3)
@@ -101,12 +102,40 @@ int	is_valid(char *file, size_t file_buffer)
 	return (1);
 }
 
+/*	Function will convert each new line char into a dot to determine
+**	shape of the piece
+*/
+
+void	*convert_to_dot(char *string_tetrino)
+{
+	int i;
+
+	i = 0;
+	while (string_tetrino[i] != '\0')
+	{
+		if (string_tetrino[i] == '\n')
+			string_tetrino[i] = ('.');
+		i++;
+	}
+	return (string_tetrino);
+}
+
+int		validate_tetrino(char *string_shape)
+{
+	convert_to_dot(string_shape);
+	if (ft_strcmp(ft_strctrim(string_shape), H_LINE) == 0)
+		return (1);
+	else
+		return (0);
+}
+
 int 	main(int argc, char **argv)
 {
-	int read_file_bytes;
 	char *file_name;
 	char *buf;
+	//char **tetrimino_in_file;
 	int return_value;
+
 
 	file_name = argv[argc - 1];
 	if (argc != 2)
@@ -115,6 +144,11 @@ int 	main(int argc, char **argv)
 		return (0);
 	}
 	buf = read_information(file_name);
-	return_value = is_valid(buf, 41);
-	printf("%d\n", return_value);
+	return_value = is_valid(buf, 41);  //need to calculate buff size
+	// if (return_value == 0)
+	// 	return (0);
+	// else 
+	//tetrimino_in_file = ft_tet_split(buf);
+	printf("%s\n", (buf));
+	printf("%d\n", ft_tit_count(buf));
 }
